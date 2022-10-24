15291a485ab2326426b03e4dad452648e96da0ea
Adrien Gregorj <adrien.gregorj@gmail.com>
2022-10-24 21:36:25 +0900

    make icon for date

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -7,6 +7,7 @@ import "@vuepic/vue-datepicker/dist/main.css";
 import {
   ChartBarIcon,
   QuestionMarkCircleIcon,
+  CalendarDaysIcon,
 } from "@heroicons/vue/24/outline";
 import StatisticsModal from "@/components/modals/StatisticsModal.vue";
 import HowToPlayModal from "@/components/modals/HowToPlayModal.vue";
@@ -43,62 +44,63 @@ const maxDate = computed(() => {
 </script>
 
 <template>
-  <header class="mb-8 flex h-32 flex-col items-center space-y-4 py-2 px-4">
-    <div class="flex w-full items-center justify-center">
-      <div class="flex w-full max-w-md items-center justify-between">
-        <div class="flex space-x-2">
-          <div class="cursor-pointer" @click="modalStore.toggleStatistics()">
-            <ChartBarIcon
-              class="h-6 w-8 text-indigo-500 transition duration-100 ease-in-out hover:text-indigo-300"
-            />
-          </div>
+  <header class="mb-8 flex w-full items-center justify-center py-2 px-4">
+    <div class="flex w-full max-w-md items-center justify-between">
+      <div class="flex space-x-2">
+        <div class="cursor-pointer" @click="modalStore.toggleStatistics()">
+          <ChartBarIcon
+            class="h-6 w-8 text-indigo-500 transition duration-100 ease-in-out hover:text-indigo-300"
+          />
         </div>
-        <div
-          @mouseenter="randomShuffle"
-          @mouseleave="reset"
-          class="group flex bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-center font-mono text-5xl font-bold text-transparent"
-        >
-          <div
-            v-for="(letter, i) in letters"
-            :key="i"
-            class="w-8 transition duration-700 ease-in-out"
-            :class="{
-              'translate-x-0': translations[i] == 0,
-              'translate-x-8': translations[i] == 1,
-              'translate-x-16': translations[i] == 2,
-              'translate-x-24': translations[i] == 3,
-              'translate-x-32': translations[i] == 4,
-              'translate-x-40': translations[i] == 5,
-              '-translate-x-8': translations[i] == -1,
-              '-translate-x-16': translations[i] == -2,
-              '-translate-x-24': translations[i] == -3,
-              '-translate-x-32': translations[i] == -4,
-              '-translate-x-40': translations[i] == -5,
-            }"
-          >
-            {{ letter }}
-          </div>
-        </div>
-        <div class="flex space-x-2">
-          <div class="cursor-pointer" @click="modalStore.toggleHowToPlay()">
-            <QuestionMarkCircleIcon
-              class="h-6 w-8 text-pink-500 transition duration-100 ease-in-out hover:text-pink-300"
-            />
-          </div>
+        <div class="cursor-pointer">
+          <Datepicker
+            v-model="historyStore.date"
+            :minDate="minDate"
+            :maxDate="maxDate"
+            :enableTimePicker="false"
+            :clearable="false"
+            :auto-apply="true"
+            :highlight="historyStore.getDatesPlayed"
+            preventMinMaxNavigation
+            ><template #trigger
+              ><CalendarDaysIcon
+                class="h-6 w-8 text-indigo-500 transition duration-100 ease-in-out hover:text-indigo-300" /></template
+          ></Datepicker>
         </div>
       </div>
-    </div>
-    <div>
-      <Datepicker
-        v-model="historyStore.date"
-        :minDate="minDate"
-        :maxDate="maxDate"
-        :enableTimePicker="false"
-        :clearable="false"
-        :auto-apply="true"
-        :highlight="historyStore.getDatesPlayed"
-        preventMinMaxNavigation
-      />
+      <div
+        @mouseenter="randomShuffle"
+        @mouseleave="reset"
+        class="group flex bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-center font-mono text-5xl font-bold text-transparent"
+      >
+        <div
+          v-for="(letter, i) in letters"
+          :key="i"
+          class="w-8 transition duration-700 ease-in-out"
+          :class="{
+            'translate-x-0': translations[i] == 0,
+            'translate-x-8': translations[i] == 1,
+            'translate-x-16': translations[i] == 2,
+            'translate-x-24': translations[i] == 3,
+            'translate-x-32': translations[i] == 4,
+            'translate-x-40': translations[i] == 5,
+            '-translate-x-8': translations[i] == -1,
+            '-translate-x-16': translations[i] == -2,
+            '-translate-x-24': translations[i] == -3,
+            '-translate-x-32': translations[i] == -4,
+            '-translate-x-40': translations[i] == -5,
+          }"
+        >
+          {{ letter }}
+        </div>
+      </div>
+      <div class="flex space-x-2">
+        <div class="cursor-pointer" @click="modalStore.toggleHowToPlay()">
+          <QuestionMarkCircleIcon
+            class="h-6 w-8 text-pink-500 transition duration-100 ease-in-out hover:text-pink-300"
+          />
+        </div>
+      </div>
     </div>
   </header>
 
